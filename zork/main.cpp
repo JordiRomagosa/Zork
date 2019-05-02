8af1a1f1aea0e3df6daaacb0b2234dbb73c8ab19
Jordi <jordi@shiraz.es>
2019-05-02 11:07:08 +0200

    player can write commands, answer is input

--- zork/main.cpp
+++ zork/main.cpp
@@ -1,8 +1,40 @@
-#include <iostream> 
+#include <iostream>
+#include <string>
+#include <conio.h>
+
 using namespace std;
 
 int main()
 {
-	cout << "Hello World! ";
+	char key;
+	string player_input = "";
+
+	while (1)
+	{
+		if (_kbhit() != 0)
+		{
+			key = _getch();
+			if (key == '\b' && player_input.length() > 0) //back
+			{
+				player_input.pop_back();
+				cout << '\b';
+				cout << " ";
+				cout << '\b';
+			}
+			
+			else if (key == '\r') // return
+			{
+				player_input += "\n";
+				cout << "\n" + player_input;
+				player_input = "";
+			}
+			else
+			{
+				player_input.push_back(key);
+				cout << key;
+			}
+		}
+	}
+
 	return 0;
 }
