fc5bad4a33a5b0761a041012bc3aa44625525979
Jordi <jordi@shiraz.es>
2019-05-02 12:03:14 +0200

    Added global utility functions

---
+++ zork/globals.h
@@ -0,0 +1,16 @@
+#ifndef __Globals__
+#define __Globals__
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool Same(const string& a, const string& b);
+bool Same(const char* a, const string& b);
+bool Same(const string& a, const char* b);
+int Random(int min, int max);
+void Tokenize(const string& line, vector<string>& arguments);
+
+#endif //__Globals__
+
